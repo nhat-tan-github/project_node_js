d34d5059cd833eef1c731acf6bc4f2277ead7601
nhat-tan-github <hehhn899@gmail.com>
2025-05-24 00:00:43 +0700

    cap nhat env tao view voi index.hbs cung style thay  send bang render

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const express = require("express");
+const path = require('path');
 const mysql = require("mysql2");
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env'});
 
 const app = express();
 
 // ket noi database
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'nodejs-login'
-})
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+app.set('view engine', 'hbs');
 
 db.connect((error) => {
     if (error) {
-        console.log(error)
+        console.log(error);
     } else {
-        console.log("Mysql connected ...")
+        console.log("Mysql connected ...");
     }
 })
 
 app.get("/", (req, res) => {
-    res.send("<h1>Home Page</h1>");
+    // res.send("<h1>Home Page</h1>");
+    res.render("index");
 })
 
 app.listen(5001, () => {
