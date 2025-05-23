f08a22ecf77f79f0fe615cbafed7911565925b53
nhat-tan-github <hehhn899@gmail.com>
2025-05-23 23:14:16 +0700

    ket noi database doi driver mysql  sang mysql2

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const app = express();
+
+// ket noi database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'nodejs-login'
+})
+
+db.connect((error) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log("Mysql connected ...")
+    }
+})
 
 app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>");
