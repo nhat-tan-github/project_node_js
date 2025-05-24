7d09d593aecbf829c323e23d03762edf237a46de
nhat-tan-github <hehhn899@gmail.com>
2025-05-24 11:37:55 +0700

    feat(auth): hoàn thiện chức năng đăng ký người dùng
    - Hash mật khẩu với bcrypt
    - Kiểm tra email đã tồn tại
    - Thêm hiển thị thông báo lỗi trong giao diện đăng ký (register.hbs)

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const path = require('path');
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './.env'});
 
 const app = express();
 
-// ket noi database
-const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
-});
+// // ket noi database
+// const db = mysql.createConnection({
+//     host: process.env.DATABASE_HOST,
+//     user: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASSWORD,
+//     database: process.env.DATABASE
+// });
 
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
@@ -25,13 +25,13 @@ app.use(express.json());
 
 app.set('view engine', 'hbs');
 
-db.connect((error) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Mysql connected ...");
-    }
-})
+// db.connect((error) => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log("Mysql connected ...");
+//     }
+// })
 
 //Define Routes
 app.use('/', require('./routes/pages.js'));
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,63 @@
+const mysql = require("mysql2");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+// ket noi database
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
 
-
-
-
+db.connect((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Mysql connected ...");
+    }
+});
 
 exports.register = (req, res) => {
     console.log(req.body);
-    res.send("Form submitted");
+
+    // const name = req.body.name;
+    // const email = req.body.email;
+    // const password = req.body.password;
+    // const passwordConfirm = req.body.passwordConfirm;
+
+    const {name, email, password, passwordConfirm} = req.body;
+    db.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => {
+        if(error) {
+            console.log(error);
+        }
+
+         if(results.length > 0) {
+           return res.render('register', {
+            message: 'That email is already in use'
+           });
+        } else if(password !== passwordConfirm) {
+            return res.render('register', {
+            message: 'Passwords do not match'
+            });
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
+
+        db.query('INSERT INTO users SET ?', {
+            name: name,
+            email: email,
+            password: hashedPassword
+        }, (error, results) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(results);
+                return res.render('register', {
+                    message: 'User registered'
+                });
+            }
+        });
+    });
 }
